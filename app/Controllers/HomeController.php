54687496b3f78d1587236324283bde3575e5ceeb
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2024-03-13 15:27:30 -0300

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1522,7 +1522,7 @@ class HomeController
 		$diagnostico->fieldTypes("profesional", "select");
 		//$diagnostico->fieldDataBinding("profesional", "profesional", "id_profesional", array("nombre_profesional","apellido_profesional"), "db", " ");
 		$diagnostico->fieldAddOnInfo("diagnostico", "after", '<div class="input-group-append"><span class="btn btn-default border eliminar_diagnostico" id="basic-addon1"><i class="fa fa-trash"></i></span></div>');
-		$diagnostico->fieldAddOnInfo("profesional", "after", '<div class="input-group-append"><span class="btn btn-default border agregar_profesional" id="basic-addon1"><i class="fa fa-plus"></i></span></div>');
+		$diagnostico->fieldAddOnInfo("profesional", "after", '<div class="input-group-append"><span class="btn btn-default border agregar_profesional" data-intro="Si desea Agregar mas Profesionales que no existan en el listado, puede hacerlo presionando este botón. " id="basic-addon1"><i class="fa fa-plus"></i></span></div>');
 		$diagnostico->formFields(array("especialidad","profesional","diagnostico","sintomas_principales", "diagnostico_libre"));
 		$diagnostico->fieldRenameLable("diagnostico", "Diagnóstico CIE-10");
 		$diagnostico->fieldRenameLable("sintomas_principales", "Síntomas Principales");
